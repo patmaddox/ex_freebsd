71c56047a8bd9da2d41682f6b33db1382d36e3e4
Pat Maddox <pat@patmaddox.com>
2022-05-16 21:13:23 +0000

    Package info

--- mix.exs
+++ mix.exs
@@ -4,10 +4,15 @@ defmodule FreeBSD.MixProject do
   def project do
     [
       app: :freebsd,
+      name: "ExFreeBSD",
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description(),
+      source_url: "https://github.com/patmaddox/ex_freebsd",
+      homepage_url: "https://github.com/patmaddox/ex_freebsd"
     ]
   end
 
@@ -24,5 +29,16 @@ defmodule FreeBSD.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
+  end
+
+  defp package do
+    [
+      licenses: ["BSD-2-Clause-FreeBSD"],
+      links: %{"GitHub" => "https://github.com/patmaddox/ex_freebsd"}
+    ]
+  end
+
+  defp description do
+    "mix tasks for converting Elixir projects to FreeBSD packages."
   end
 end
