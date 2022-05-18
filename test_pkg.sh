eb33a3f659184302bb63d1744b9a1845c43f7d6d
Pat Maddox <pat@patmaddox.com>
2022-05-18 03:37:41 +0000

    CI config

---
+++ test_pkg.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -e
+
+rm -f freebsd/freebsd-*.pkg
+mix deps.get
+mix freebsd.gen.pkg
+echo "y" | mix freebsd.pkg
+pkg install -y freebsd/freebsd-*.pkg
+
+echo "Enabling service..."
+service freebsd enable
+
+echo "Checking status..."
+set +e
+service freebsd status
+result=$?
+set -e
+if [ $result -ne 1 ]
+then
+    echo "Status should exit 1 when not running; Got $result instead"
+    exit 1
+fi
+
+echo "Starting service..."
+service freebsd start
+sleep 0.2
+echo "Checking status..."
+service freebsd status
+result=$?
+if [ $result -ne 0 ]
+then
+    echo "Status should exit 0 when running; Got $result instead"
+    exit 1
+fi
+
+echo "All systems go!"
