e34c2394679e4023a8486e0cf63c14c6577bda35
Pat Maddox <pat@patmaddox.com>
2022-06-22 14:23:46 -0700

    Test service restart

--- test_pkg.sh
+++ test_pkg.sh
@@ -42,4 +42,30 @@ then
   exit 1
 fi
 
+echo "Stopping service..."
+service freebsd stop
+sleep 1
+echo "Checking status..."
+set +e
+service freebsd status
+result=$?
+set -e
+if [ $result -ne 1 ]
+then
+    echo "Status should exit 1 when not running; Got $result instead"
+    exit 1
+fi
+
+echo "Retarting service..."
+service freebsd restart
+sleep 1
+echo "Checking status..."
+service freebsd status
+result=$?
+if [ $result -ne 0 ]
+then
+    echo "Status should exit 0 when running; Got $result instead"
+    exit 1
+fi
+
 echo "All systems go!"
