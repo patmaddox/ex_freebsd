1f6adb2798f1f3f7aab7407a686c2117762e6ce5
Pat Maddox <pat@patmaddox.com>
2022-06-22 14:38:39 -0700

    Version bump

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FreeBSD.MixProject do
     [
       app: :freebsd,
       name: "ExFreeBSD",
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
